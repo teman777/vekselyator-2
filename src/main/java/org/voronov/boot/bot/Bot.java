2c98adb5ea8e351accb8a5139430a356a6dd2659
Artem Voronov <a.voronov@haulmont.com>
2021-11-26 12:30:02 +0000

    retranslate - feature for rofl

--- src/main/java/org/voronov/boot/bot/Bot.java
+++ src/main/java/org/voronov/boot/bot/Bot.java
@@ -2,8 +2,9 @@ package org.voronov.boot.bot;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.voronov.boot.core.AbstractInlineCommandBot;
 
 @Component
@@ -15,15 +16,33 @@ public class Bot extends AbstractInlineCommandBot {
     @Value("${telegram.bot.token}")
     private String token;
 
+    @Value("${telegram.bot.mainuser}")
+    private Long mainUser;
+
+    @Value("${telegram.bot.mainchat}")
+    private Long mainChat;
+
+    private Boolean isTranslateOn = Boolean.FALSE;
+
     @Override
     public void processNonCommandAndInlineUpdate(Update update) {
-        if (update.getMessage().isReply()) {
-            handleReply(update.getMessage());
+        if (update.getMessage().getChat().isUserChat()) {
+            handleRetranslation(update);
         }
     }
 
-    private void handleReply(Message message) {
-        //commandService.handleReply(message, this);
+    private void handleRetranslation(Update update) {
+        if (isTranslateOn && update.getMessage().getFrom().getId().equals(mainUser)) {
+            SendMessage sm = SendMessage.builder()
+                    .chatId(mainChat.toString())
+                    .text(update.getMessage().getText())
+                    .build();
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -34,5 +53,13 @@ public class Bot extends AbstractInlineCommandBot {
     @Override
     public String getBotToken() {
         return token;
+    }
+
+    public Boolean getTranslateOn() {
+        return isTranslateOn;
+    }
+
+    public void setTranslateOn(Boolean translateOn) {
+        isTranslateOn = translateOn;
     }
 }
