961406a9c9fe79b5a58dc31744bbdfdfbe451bc3
Artem Voronov <a.voronov@haulmont.com>
2022-04-23 16:10:33 +0100

    emoji group name fix

--- db/alter/alter_2.sql
+++ db/alter/alter_2.sql
@@ -1,2 +1,3 @@
 alter table Chats convert to character set utf8mb4 collate utf8mb4_bin;
 alter table Operations convert to character set utf8mb4 collate utf8mb4_bin;
+alter table Users convert to character set utf8mb4 collate utf8mb4_bin;
